e1359620547aa6e79912bf5e87ecde303f222fc1
Muhammad-Aqib07 <khanson0543@gmail.com>
2023-08-22 20:36:47 +0500

    A variable is appeared in this change ok

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+var a = 60;
+if (a == 80 && 80 < a) {
+    console.log("Student Grade is A");
+}
+else if (a >= 70 && 80 > a) {
+    console.log("Student Grade B");
+}
+else if (a >= 60 && 70 > a) {
+    console.log("Student Grade c");
+}
+else
+    (a >= 50);
---
+++ index.ts
@@ -0,0 +1,14 @@
+var a: number = 60;
+if(a ==80 && 80<a){
+    console.log("Student Grade is A")
+}
+else if(a>=70 && 80>a){
+console.log("Student Grade B")
+}
+else if(a>=60 && 70>a){
+    console.log("Student Grade c")
+}
+else(a>=50)
+// this is new project created by my me now 
+var D:string[]=["alam","saadi","qutab "]
+console.log(D)
